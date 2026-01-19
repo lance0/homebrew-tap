af05139976b4b7e6e527846155ef5a6ab66012e4
Lance Tuller <lancey3@gmail.com>
2026-01-19 12:13:07 -0500

    ttl 0.12.7: Fix non-responding hops, add Ctrl+C quit

--- Formula/ttl.rb
+++ Formula/ttl.rb
@@ -1,24 +1,24 @@
 class Ttl < Formula
   desc "Modern traceroute/mtr-style TUI with hop stats and ASN/geo enrichment"
   homepage "https://github.com/lance0/ttl"
-  version "0.12.6"
+  version "0.12.7"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/lance0/ttl/releases/download/v#{version}/ttl-aarch64-apple-darwin.tar.gz"
-      sha256 "d5d9492494a145a15132b7b70a9d05a89ce5b82051888dbe12e124e60bc82f8a"
+      sha256 "bdc41a237b12c84ee9601481f0e79568e79943daf55621488e7867dbc6ac6f3e"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/lance0/ttl/releases/download/v#{version}/ttl-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "5e71f1f1dc936f6d610343fc640911adeaede8b94620b1d22f6d834938f4fe1e"
+      sha256 "a67bd37e1a1507bb9e4eb262697a2118eb478148d483f18280a4360a5afece5c"
     end
     on_arm do
       url "https://github.com/lance0/ttl/releases/download/v#{version}/ttl-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "80ba048ff286f3a7058fd5496f9d1544051ffe39c31b5cca9afd6c4514e93f4d"
+      sha256 "5ef79f899d83fb9b037e36eff08e44f0265ffe065a151a560aca3f3d2a6e1328"
     end
   end
 
