0101ec03343f4c394cf87b91d7f3c5e333e61d0e
Lance Tuller <lancey3@gmail.com>
2026-02-23 09:26:43 -0500

    Update ttl to v0.18.2

--- Formula/ttl.rb
+++ Formula/ttl.rb
@@ -1,28 +1,28 @@
 class Ttl < Formula
   desc "Modern traceroute/mtr-style TUI with hop stats and ASN/geo enrichment"
   homepage "https://github.com/lance0/ttl"
-  version "0.18.1"
+  version "0.18.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/lance0/ttl/releases/download/v#{version}/ttl-aarch64-apple-darwin.tar.gz"
-      sha256 "7b2562454734cd139b7efd69798a0c9c6b3c3b822d3ff450fc7a194f6b9e8611"
+      sha256 "31f2a1ff2f63cea5e8f5ecde143e7b3711b3a0eabc8217e18389260d8bad5730"
     end
     on_intel do
       url "https://github.com/lance0/ttl/releases/download/v#{version}/ttl-x86_64-apple-darwin.tar.gz"
-      sha256 "798e3c6e501e241a3edc9ed260579b25f209f1fcc5c84bbd3fe19ed5a3507f1b"
+      sha256 "850c54517919c6642aefeb89a8faa95b416268052da79d993bd0c967c14240a9"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/lance0/ttl/releases/download/v#{version}/ttl-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "eb462af61175a14077c55b99d36c4bf7d28f594ec28b82f7890511fc97c3b186"
+      sha256 "a54e4f76ba98f0d1c382e53031f7155087af015929d5314b731b14da85473316"
     end
     on_arm do
       url "https://github.com/lance0/ttl/releases/download/v#{version}/ttl-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "8a670b4725d555c82aa34408dc00ea8b9213a8b121998595d239d9bbe95cf5e4"
+      sha256 "805dc9cdf16ce6f5ca3b44af995a78276d7457a62c68b0d1b094179271b71bba"
     end
   end
 
