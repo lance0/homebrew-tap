35be42675e6e9a464a620b41150250ce4eca2237
Lance Tuller <lancey3@gmail.com>
2026-06-24 15:27:41 -0400

    Update nbox to v0.12.0

--- Formula/nbox.rb
+++ Formula/nbox.rb
@@ -4,28 +4,28 @@
 class Nbox < Formula
   desc "Terminal UI and CLI for NetBox"
   homepage "https://github.com/lance0/nbox"
-  version "0.11.0"
+  version "0.12.0"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     on_arm do
       url "https://github.com/lance0/nbox/releases/download/v#{version}/nbox-aarch64-apple-darwin.tar.gz"
-      sha256 "d8587584325a987b2eff3b5161947529fc024ff7fd4aae721ab5fb16c9f352a9"
+      sha256 "992e301b0eec24f9137e242819a4992fd5a33958bb08576d2200a0f21601aca2"
     end
     on_intel do
       url "https://github.com/lance0/nbox/releases/download/v#{version}/nbox-x86_64-apple-darwin.tar.gz"
-      sha256 "b52073b76b44330a581938318ebffaf887e8f382d1fd2628ba38650b27955ba4"
+      sha256 "3a0ad3441c56c96ffd40f128ad0d04d922033b92591fcb4ecb3c97435174f6d5"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/lance0/nbox/releases/download/v#{version}/nbox-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "8ab745086c25a46276c43ae6762ace9b501d7620d8bae4966fcc5abb6bd4e488"
+      sha256 "165ecd7f513d5c622a374f9f1933ff186ec0ca555897f0aa3a8cb800e1f33cfb"
     end
     on_intel do
       url "https://github.com/lance0/nbox/releases/download/v#{version}/nbox-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "dd091086d910fdde3c4c8833dd7409b8de6bc51ac2db6ff0cb912823895aa8b3"
+      sha256 "b9f5216a0badb7e00e0f92d982fe9f6b05afcec57c822c26c818d6d93b5b9853"
     end
   end
 
